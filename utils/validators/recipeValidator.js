fb3732e016133e7bc57a567054e13ddfdb9e687e
Fadyy22 <fadyalaa441@gmail.com>
2023-11-30 18:36:13 +0200

    add recipe validation to check if subcategory exists in the selected category

--- utils/validators/recipeValidator.js
+++ utils/validators/recipeValidator.js
@@ -5,6 +5,9 @@ const Category = require('../../models/categoryModel');
 const SubCategory = require('../../models/subCategoryModel');
 const Recipe = require('../../models/recipeModel');
 
+
+let category;
+
 exports.getRecipesValidator = [
   check('categoryId')
     .optional()
@@ -48,6 +51,7 @@ exports.createRecipeValidator = [
     .isMongoId()
     .withMessage('Invalid category id format.')
     .custom(categoryId => {
+      category = categoryId;
       return Category.findById(categoryId).
         then(category => {
           if (!category) {
@@ -64,6 +68,9 @@ exports.createRecipeValidator = [
         then(subcategory => {
           if (!subcategory) {
             return Promise.reject(new Error('Subcategory not found.'));
+          }
+          if (subcategory.category.toString() !== category.toString()) {
+            return Promise.reject(new Error('Subcategory does\'t exist in this category.'));
           }
         })
     }),
